e04152eb14f454d24bf05ba74c0f8603e75ab79d
Kashav Madan <kshvmdn@gmail.com>
2016-03-13 17:06:21 -0400

    Open/close application

--- main.py
+++ main.py
@@ -3,9 +3,25 @@
 import sys
 from appscript import *
 from termcolor import colored, cprint
+
+
+def open(itunes):
+    return itunes.activate()
+
+
+def close(itunes):
+    return itunes.quit()
+
+
 def main():
     cmd, is_open, itunes = None if len(sys.argv) == 1 else sys.argv[1], \
         app('System Events').processes[its.name == 'iTunes'].count(), \
         app('iTunes')
+
+    if not is_open == 1:
+        open(itunes)
+
+    return cmd(itunes)
+
 if __name__ == '__main__':
     main()
